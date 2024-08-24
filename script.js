2ec57df8d13043a523ffcfe5205bb7ab5e4d9bf8
theezanonymous <leonliu509@gmail.com>
2024-08-24 00:03:19 -0400

    Changed link

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ for(let i = 0; i < 300; i++){
         anchor.href = "https://media.tenor.com/OA8KFcZxPjsAAAAi/sad-emoji.gif";
     }
     else{
-        anchor.href = "https://docs.google.com/document/d/12-kn172Vv2M3HGMBvAgC_gStwkXX9d4Dvp75fA7v05w/edit?usp=sharing";
+        // anchor.href = "https://docs.google.com/document/d/12-kn172Vv2M3HGMBvAgC_gStwkXX9d4Dvp75fA7v05w/edit?usp=sharing";
+        anchor.href = "https://docs.google.com/document/d/1-PfKjXNivt9IfgtF1qrTzlwlC2x_47l6mDv8yLNnc4s/edit?usp=sharing";
     }
     const temp = document.createElement("button");
     temp.style.color = "black"; temp.style.fontWeight = 1000;
