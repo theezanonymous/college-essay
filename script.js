f65d27a488b0272440b0494309409c0b66b2c529
theezanonymous <leonliu509@gmail.com>
2024-09-09 07:41:09 -0400

    Fixed ui

--- script.js
+++ script.js
@@ -11,22 +11,9 @@ for(let i = 0; i < 300; i++){
     }
     const temp = document.createElement("button");
     temp.style.color = "black"; temp.style.fontWeight = 1000;
-    temp.style.backgroundColor = "cyan"; 
+
     temp.innerText = i; 
-    temp.style.minWidth = "100px"; temp.style.minHeight = "100px";
-    temp.style.borderRadius = "6px";
     temp.style.textWrap = "flexwrap"; 
-    temp.addEventListener("mouseenter", ()=>{
-        temp.style.borderWidth = "10px";
-        temp.style.maxWidth = "120px"; temp.style.maxHeight = "120px";
-       temp.style.minWidth = "120px"; temp.style.minHeight = "120px";
-    });
-    temp.addEventListener("mouseleave",()=>{
-        temp.style.borderWidth = "6px";
-        temp.style.minWidth = "100px"; temp.style.minHeight = "100px";
-        temp.style.maxWidth = "100px"; temp.style.maxHeight = "100px";
-    }
-    );
     anchor.appendChild(temp);
     container.appendChild(anchor);
 }
