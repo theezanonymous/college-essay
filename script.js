e3438e61bcf3017333231445be7ae88aff1691f5
theezanonymous <leonliu509@gmail.com>
2024-09-09 07:48:37 -0400

    Fix box color

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ const container = document.querySelector("#container");
 const birthday = undefined;
 for(let i = 0; i < 300; i++){
     const anchor = document.createElement("a");
+    anchor.className = "special";
     if(i !=82){
         anchor.href = "https://media.tenor.com/OA8KFcZxPjsAAAAi/sad-emoji.gif";
     }
     else{
-        // anchor.href = "https://docs.google.com/document/d/12-kn172Vv2M3HGMBvAgC_gStwkXX9d4Dvp75fA7v05w/edit?usp=sharing";
+        anchor.className = "specialBox";
         anchor.href = "https://docs.google.com/document/d/1-PfKjXNivt9IfgtF1qrTzlwlC2x_47l6mDv8yLNnc4s/edit?usp=sharing";
     }
     const temp = document.createElement("button");
     temp.style.color = "black"; temp.style.fontWeight = 1000;
+    if(i==82){
+    temp.className = "specialButton";}
 
     temp.innerText = i; 
     temp.style.textWrap = "flexwrap"; 
