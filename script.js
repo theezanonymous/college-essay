c868b38f0bc396d27b0bbdc6438ccf58a4afda25
theezanonymous <leonliu509@gmail.com>
2025-07-09 15:39:09 -0400

    Add laughing gif

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
   
+function toggleImg(){
+    let e = document.querySelector("img")
+    e.className = e.className =="show"? "hide": "show";
+}
 // "https://docs.google.com/document/d/1-PfKjXNivt9IfgtF1qrTzlwlC2x_47l6mDv8yLNnc4s/edit?usp=sharing";
 let btn = document.querySelector(".submit")
 btn.addEventListener("click", async ()=>{
@@ -12,12 +16,15 @@ btn.addEventListener("click", async ()=>{
     if(bday=="2007-08-02"){
         btn.id = "right"
         btn.innerText = "RIGHT!!"
-        delay(4000);
+        await delay(2000);
         window.location.href = "https://docs.google.com/document/d/1-PfKjXNivt9IfgtF1qrTzlwlC2x_47l6mDv8yLNnc4s/edit?usp=sharing";
     }
     else{
         btn.id = "wrong"
         btn.innerText = "WRONG!!"
+        toggleImg();
+        await delay(4000);
+        toggleImg();
     }
 
     
